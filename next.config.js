8f6eca34bf68b9d8a06c3d3839a846f3e918b131
Victor Jesús Rosario Vásquez <46900196+Victor1890@users.noreply.github.com>
2023-12-21 15:33:30 -0400

    feat: :art: add eslint, prettier and conversational commit
    I added few feature for get the best experience of development

--- next.config.js
+++ next.config.js
@@ -2,30 +2,30 @@
  * @type {import('next').NextConfig}
  */
 
-const withPWA = require("next-pwa")({
-  dest: "public",
+const withPWA = require('next-pwa')({
+  dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-  publicExcludes: ["!resume.pdf"],
+  disable: process.env.NODE_ENV === 'development',
+  publicExcludes: ['!resume.pdf'],
 });
 
 module.exports = withPWA({
   reactStrictMode: true,
   images: {
     domains: [
-      "ucarecdn.com",
-      "cdn.buymeacoffee.com",
-      "res.cloudinary.com",
-      "imgur.com",
-      "i.imgur.com",
-      "cutt.ly",
-      "activity-graph.herokuapp.com",
-      "i.scdn.co",
-      "images.unsplash.com",
-      "m.media-amazon.com",
-      "image.tmdb.org", // tmdb images,
-      "media.licdn.com"
+      'ucarecdn.com',
+      'cdn.buymeacoffee.com',
+      'res.cloudinary.com',
+      'imgur.com',
+      'i.imgur.com',
+      'cutt.ly',
+      'activity-graph.herokuapp.com',
+      'i.scdn.co',
+      'images.unsplash.com',
+      'm.media-amazon.com',
+      'image.tmdb.org', // tmdb images,
+      'media.licdn.com',
     ],
   },
   typescript: {
--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,8 +1,8 @@
-import { defineCliConfig } from "@sanity/cli";
+import { defineCliConfig } from '@sanity/cli';
 
 export default defineCliConfig({
   api: {
-    projectId: "5ec749u0",
-    dataset: "production",
+    projectId: '5ec749u0',
+    dataset: 'production',
   },
 });
