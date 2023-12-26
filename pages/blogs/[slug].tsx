5ebf08a8c2c6691a8e1c36f1918b15508273fef1
Victor Jesús Rosario Vásquez <46900196+Victor1890@users.noreply.github.com>
2023-12-26 14:08:59 -0400

    refactor: :art: refactor one blog page and page 404

--- pages/blogs/[slug].tsx
+++ pages/blogs/[slug].tsx
@@ -1,57 +1,23 @@
-import MDXComponents from '@components/MDXComponents'
-import Metadata from '@components/app/meta/MetaData'
-import BlogLayout from '@layout/BlogLayout'
+import BlogPostPage from '@components/pages/blogs/slug'
 import { IArticleDevTo } from '@provider/dev.to/devto.interface'
 import devToProvider from '@provider/dev.to/devto.provider'
 import { getMDXTableOfContents } from '@utils/mdx.util'
 import { ITableOfContentMDX } from 'interfaces/common/common.interface'
 import { GetStaticPropsContext } from 'next'
-import { MDXRemote } from 'next-mdx-remote'
 import { serialize } from 'next-mdx-remote/serialize'
-import PageNotFound from 'pages/404'
-
-export default function Post({
-	post,
-	error,
-}: {
-	post: IArticleDevTo & { content: any; tableOfContents: ITableOfContentMDX[] }
-	error: boolean
-}) {
-	if (!post || error) return <PageNotFound />
-
-	return (
-		<>
-			<Metadata
-				title={post.title}
-				suffix='Victor Rosario'
-				description={post.description}
-				previewImage={post.cover_image}
-				keywords={post.tags ?? ''}
-			/>
-
-			<BlogLayout post={post}>
-				<MDXRemote
-					{...(post.content as any)}
-					frontmatter={{
-						slug: post.slug,
-						excerpt: post.description,
-						title: post.title,
-						date: post.published_at,
-						keywords: post.tags ?? '',
-						image: post.cover_image,
-					}}
-					components={MDXComponents}
-				/>
-			</BlogLayout>
-		</>
-	)
-}
 
 type StaticProps = GetStaticPropsContext & {
 	params: {
 		slug: string
 	}
 }
+
+const PostPage = ({ post, error }: {
+	post: IArticleDevTo & { content: any; tableOfContents: ITableOfContentMDX[] }
+	error: boolean
+}) => <BlogPostPage error={error} post={post} />
+
+export default PostPage
 
 export async function getStaticProps({ params }: StaticProps) {
 	const { slug } = params
