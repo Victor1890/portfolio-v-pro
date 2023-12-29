fa12e0e375cd3deb948a8c904bb4c1349c7e58ea
Victor Jesús Rosario Vásquez <46900196+Victor1890@users.noreply.github.com>
2023-12-28 23:32:03 -0400

    refactor: ref

--- components/layout/TopNavbar.tsx
+++ components/layout/TopNavbar.tsx
@@ -100,8 +100,9 @@ function NavItem({ href, text }: { href: string; text: string }) {
 	const isActive = router.asPath === (href === '/home' ? '/' : href)
 	return (
 		<Link
-			className={`${isActive ? 'font-bold text-gray-800 dark:text-gray-100' : ' text-gray-600 dark:text-gray-300'
-				} hidden rounded-md px-2 py-[3px] text-[17px] transition-all hover:bg-black/10 sm:inline-block  md:px-3 dark:hover:bg-neutral-700/50`}
+			className={`${
+				isActive ? 'font-bold text-gray-800 dark:text-gray-100' : ' text-gray-600 dark:text-gray-300'
+			} hidden rounded-md px-2 py-[3px] text-[17px] transition-all hover:bg-black/10 sm:inline-block  md:px-3 dark:hover:bg-neutral-700/50`}
 			href={href === '/home' ? '/' : href}
 		>
 			<motion.p className='capitalize' variants={popUp}>
--- pages/api/now-playing.ts
+++ pages/api/now-playing.ts
@@ -2,18 +2,18 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import spotifyProvider from '@provider/spotify/spotify.provider'
 
 export default async function handler(_req: NextApiRequest, res: NextApiResponse) {
-	const song = await spotifyProvider.getCurrentlyPlayingSong().catch(() => null);
+	const song = await spotifyProvider.getCurrentlyPlayingSong().catch(() => null)
 
 	if (!song) {
-		return res.status(200).json({ isPlaying: false });
+		return res.status(200).json({ isPlaying: false })
 	}
 
-	const isPlaying = song.is_playing;
-	const title = song.item.name;
-	const artist = song.item.artists.map((artist) => artist.name).join(', ');
-	const album = song.item.album.name;
-	const albumImageUrl = song.item.album.images[0].url;
-	const songUrl = song.item.external_urls.spotify;
+	const isPlaying = song.is_playing
+	const title = song.item.name
+	const artist = song.item.artists.map((artist) => artist.name).join(', ')
+	const album = song.item.album.name
+	const albumImageUrl = song.item.album.images[0].url
+	const songUrl = song.item.external_urls.spotify
 
 	return res.status(200).json({
 		album,
@@ -22,5 +22,5 @@ export default async function handler(_req: NextApiRequest, res: NextApiResponse
 		isPlaying,
 		songUrl,
 		title,
-	});
+	})
 }
