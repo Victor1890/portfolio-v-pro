b0163ca7ab91968d91f62028f667ef37e8dd34e3
Victor Jesús Rosario Vásquez <46900196+Victor1890@users.noreply.github.com>
2024-01-01 19:46:25 -0400

    refactor: update dependecy

--- components/MDXComponents/Code.tsx
+++ components/MDXComponents/Code.tsx
@@ -1,14 +1,14 @@
 import { CSSProperties, useEffect, useState } from 'react'
 import { Prism as SyntaxHighlighter, SyntaxHighlighterProps } from 'react-syntax-highlighter'
 
-export default function Code({ children }: SyntaxHighlighterProps) {
+export default function Code({ children, language = "typescript" }: SyntaxHighlighterProps) {
 	const [styles, setStyles] = useState<{ [key: string]: CSSProperties } | undefined>(undefined)
 	useEffect(() => {
 		import('react-syntax-highlighter/dist/esm/styles/prism/one-dark').then((mod) => setStyles(mod.default))
 	}, [])
 
 	return (
-		<SyntaxHighlighter language={'typescript'} style={styles}>
+		<SyntaxHighlighter language={language} style={styles}>
 			{children}
 		</SyntaxHighlighter>
 	)
