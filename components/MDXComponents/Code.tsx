6bd392e1a5905892c56112d3aa07a31c73d03834
Victor Jesús Rosario Vásquez <46900196+Victor1890@users.noreply.github.com>
2024-02-04 18:44:56 -0400

    fix: :bug: refactor code and update vercel.json

--- components/MDXComponents/Code.tsx
+++ components/MDXComponents/Code.tsx
@@ -7,8 +7,8 @@ interface ICodeProps {
 export default function Code({ children }: ICodeProps) {
 	return (
 		<Fragment>
-			{typeof children === "string" ? (
-				<code className="!bg-gray-500 p-0.5 rounded before:text-gray-500 after:text-gray-500 text-white">
+			{typeof children === 'string' ? (
+				<code className='rounded !bg-gray-500 p-0.5 text-white before:text-gray-500 after:text-gray-500'>
 					{children}
 				</code>
 			) : (
