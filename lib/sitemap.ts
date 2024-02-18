8bd19f2bf3521e021b3554e16be6d26c444509a5
Victor Jesús Rosario Vásquez <46900196+Victor1890@users.noreply.github.com>
2024-02-18 18:58:21 -0400

    refactor: :arrow_up: update next.js package

--- lib/sitemap.ts
+++ lib/sitemap.ts
@@ -1,21 +1,14 @@
 import { globby } from 'globby'
 import { writeFileSync } from 'fs'
 import config from '@config'
-import devToProvider from '@provider/dev.to/devto.provider'
+import devToJson from '@content/data/dev.to-posts.json'
 
 const { appUrl } = config
 
 export default async function generate() {
 	const pages = await globby(['pages/*.tsx', '!pages/_*.tsx', '!pages/api', '!pages/404.tsx'])
 
-	const blogs = await devToProvider.getPageOfPosts().catch((e) => {
-		console.error('devToProvider.getPageOfPosts error: ', e)
-		return null
-	})
-
-	if (!blogs) return
-
-	const postsSlugs = blogs.map((item) => `/blogs/${item.slug}`)
+	const postsSlugs = devToJson.map((item) => `/blogs/${item.slug}`)
 
 	const pagesRoute = pages.map((page) => {
 		const path = page.replace('pages', '').replace('.tsx', '')
@@ -26,14 +19,14 @@ export default async function generate() {
 	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         ${[...pagesRoute, ...postsSlugs]
-					.map((route) => {
-						return `
+			.map((route) => {
+				return `
               <url>
                   <loc>${`${appUrl}${route}`}</loc>
               </url>
             `
-					})
-					.join('')}
+			})
+			.join('')}
     </urlset>
     `
 
