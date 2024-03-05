d5e853f8f725fc9d9f117fdefcf22b76807c79f2
Victor1890 <victorrosariodeveloper@gmail.com>
2024-03-05 16:29:26 -0400

    refactor: :recycle: refactor sitemap generation code

--- src/app/blogs/sitemap.ts
+++ src/app/blogs/sitemap.ts
@@ -5,21 +5,22 @@ import { MetadataRoute } from 'next'
 const { appUrl } = config
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+	const data = await devToProvider.getPageOfPosts().catch(() => null)
+	if (!data) {
+		return [
+			{
+				url: appUrl,
+				lastModified: new Date(),
+				changeFrequency: 'yearly',
+				priority: 1,
+			},
+		]
+	}
 
-    const data = await devToProvider.getPageOfPosts().catch(() => null)
-    if (!data) {
-        return [{
-            url: appUrl,
-            lastModified: new Date(),
-            changeFrequency: 'yearly',
-            priority: 1,
-        }]
-    }
-
-    return data.map(({ slug, published_at }) => ({
-        url: `${appUrl}/blogs/${slug}`,
-        lastModified: new Date(published_at),
-        changeFrequency: 'monthly',
-        priority: 0.8,
-    }))
+	return data.map(({ slug, published_at }) => ({
+		url: `${appUrl}/blogs/${slug}`,
+		lastModified: new Date(published_at),
+		changeFrequency: 'monthly',
+		priority: 0.8,
+	}))
 }
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,11 +1,3 @@
 export const homeProfileImage: string = 'https://i.imgur.com/nCVESbL.png'
 
-export const navigationRoutes: string[] = [
-	'home',
-	'about',
-	'stats',
-	'utilities',
-	'blogs',
-	'certificates',
-	'projects'
-]
+export const navigationRoutes: string[] = ['home', 'about', 'stats', 'utilities', 'blogs', 'certificates', 'projects']
