db134fbdbdd62af160c2ef07c7dbfbfad51ef1db
Victor1890 <victorrosariodeveloper@gmail.com>
2024-03-05 16:28:52 -0400

    refactor: :recycle: update .gitignore and package.json, delete next-sitemap.config.js
    public/robots.txt, modify src/utils/utils.ts

---
+++ src/app/sitemap.ts
@@ -0,0 +1,50 @@
+import { MetadataRoute } from 'next'
+import config from '@config'
+import { navigationRoutes } from '@utils/utils'
+import devToProvider from '@provider/dev.to/devto.provider'
+
+const { appUrl } = config
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+
+    const sitemapCustom: MetadataRoute.Sitemap = [
+        {
+            url: `${appUrl}/blogs/sitemap.xml`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        }
+    ]
+
+    const main: MetadataRoute.Sitemap = navigationRoutes.map(route => {
+        if (route === 'home') {
+            return {
+                url: appUrl,
+                lastModified: new Date(),
+                changeFrequency: 'yearly',
+                priority: 1,
+            }
+        }
+
+        return {
+            url: `${appUrl}/${route}`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        }
+    })
+
+    const data = await devToProvider.getPageOfPosts().catch(() => null)
+    if (!data) return main
+
+    const posts: MetadataRoute.Sitemap = data.map(({ slug, published_at }) => ({
+        url: `${appUrl}/blogs/${slug}`,
+        lastModified: new Date(published_at),
+        changeFrequency: 'monthly',
+        priority: 0.8,
+    }))
+
+
+
+    return main.concat([...posts, ...sitemapCustom])
+}
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,4 @@
-export const DEFAULT_IMAGE_URL: string = 'https://imgur.com/5dYYce8.png'
-export const AvatarImage: string = 'https://imgur.com/VAXEwKT.png'
 export const homeProfileImage: string = 'https://i.imgur.com/nCVESbL.png'
-// export const homeProfileImage = "/profile-default.webp";
 
 export const navigationRoutes: string[] = [
 	'home',
@@ -10,14 +7,5 @@ export const navigationRoutes: string[] = [
 	'utilities',
 	'blogs',
 	'certificates',
-	'projects',
-	// 'newsletter',
-	'rss',
+	'projects'
 ]
-
-export const snippetsImages: { [key: string]: string } = {
-	css: 'https://imgur.com/ArD8JIg.png',
-	js: 'https://imgur.com/lFKi8mB.png',
-	react: 'https://imgur.com/m2jv6MK.png',
-	ts: 'https://imgur.com/Ux6L5Uh.png',
-}
