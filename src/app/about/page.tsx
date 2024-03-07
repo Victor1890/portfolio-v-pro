fb3c7e1248d13e4a9d54112a2410beb963e9a311
Victor1890 <victorrosariodeveloper@gmail.com>
2024-03-07 17:45:36 -0400

    feat: :sparkles: add metadata for about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,6 +2,50 @@ import PageNotFoundPage from '@components/pages/404'
 import AboutPage from '@components/pages/about'
 import { IStaticInfo } from '@interfaces/about/about.interface'
 import MDXContent from '@lib/MDXContent'
+import type { Metadata } from 'next'
+import { metadata as meta } from '@content/metadata'
+import config from '@config'
+
+export const metadata: Metadata = {
+	title: meta.about.title,
+	description: meta.about.description,
+	keywords: meta.about.keywords,
+	authors: {
+		name: config.personName
+	},
+	creator: config.personName,
+	generator: 'Next.js',
+	alternates: {
+		canonical: config.appUrl,
+	},
+	robots: 'index, follow',
+	openGraph: {
+		type: 'website',
+		locale: 'en_US',
+		url: `${config.appUrl}/about`,
+		title: meta.about.title,
+		description: meta.about.description,
+		siteName: `Portfolio | ${config.personName}`,
+		images: [
+			{
+				url: `${config.appUrl}/open-graph/og.png`,
+				alt: meta.about.title,
+			},
+		],
+	},
+	icons: {
+		icon: "/favicon-dark.ico",
+		shortcut: "/favicon-dark.ico",
+		apple: "/apple-touch-icon.ico"
+	}
+}
+
+export default async function Page() {
+	const about = await getAboutMeInfo()
+	if (!about) return <PageNotFoundPage />
+
+	return <AboutPage about={about} />
+}
 
 async function getAboutMeInfo(): Promise<IStaticInfo | null> {
 	const data = await new MDXContent('static_pages').getPostFromSlug('about').catch((err) => {
@@ -23,11 +67,4 @@ async function getAboutMeInfo(): Promise<IStaticInfo | null> {
 	}
 
 	return about
-}
-
-export default async function Page() {
-	const about = await getAboutMeInfo()
-	if (!about) return <PageNotFoundPage />
-
-	return <AboutPage about={about} />
 }
